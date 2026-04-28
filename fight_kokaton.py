d84cfb0942d84462629d2b8db0f409e5560d6e55
yagiriku <c0a25246a9@edu.teu.ac.jp>
2026-04-28 14:34:40 +0900

    練習3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -170,6 +170,9 @@ def main():
                 if beam.rct.colliderect(bomb.rct): # 練習2
                     beam = None
                     bomb = None
+                    bird.change_img(6, screen)
+                    pg.display.update()
+                    time.sleep(1)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
